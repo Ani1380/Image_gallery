bf3a00552a90a816791adb8f4af952c635f9fb33
Anirban Karmakar <38663674+Ani1380@users.noreply.github.com>
2022-11-12 20:38:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const panels = document.querySelectorAll(".cards");
+
+panels.forEach((panel) => {
+   panel.addEventListener('mouseover', () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
